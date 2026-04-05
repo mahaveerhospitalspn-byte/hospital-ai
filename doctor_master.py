ac44137ce8bd624c9e236a1adc2c44cc21b63e14
Siddharth Rastogi <mahaveerhospitalspn@gmail.com>
2026-04-05 13:29:50 +0530

    auto update

--- doctor_master.py
+++ doctor_master.py
@@ -8,7 +8,7 @@ DOCTOR_DATABASE = {
         "degree": "MBBS, MS (Orthopaedics)",
         "reg_no": "UPMCI-67890"
     }
-
+}
 DOCTOR_DATABASE = {
     "DR SIDDHARTH RASTOGI": {
         "degree": "MBBS, D.Ortho",
