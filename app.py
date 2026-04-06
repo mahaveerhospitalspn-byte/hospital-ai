7a2777f2cad99fb7a7da093a16046239b6e12317
Siddharth Rastogi <mahaveerhospitalspn@gmail.com>
2026-04-06 18:44:09 +0530

    auto update

--- app.py
+++ app.py
@@ -27,11 +27,7 @@ from supabase_client import supabase
 
 
 
-SUPABASE_URL = "https://ptkdegqftfcaqrvsbihk.supabase.co"
 
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0a2RlZ3FmdGZjYXFydnNiaWhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMjYzODUsImV4cCI6MjA5MDcwMjM4NX0.jI2mcxJ86uPaCExOmLEdN8XdEzctEul3-33Qc7Ug_dI"
-
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 
 
@@ -3766,7 +3762,7 @@ from opd_documentation import create_drug_master, import_large_drug_dataset
 
 
 
-
+from supabase import create_client
 
 SUPABASE_URL = "https://ptkdegqftfcaqrvsbihk.supabase.co"
 
