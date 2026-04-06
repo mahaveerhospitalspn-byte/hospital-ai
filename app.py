6956e23a1d9a039a9e7a2dbf7854038617164cdb
Siddharth Rastogi <mahaveerhospitalspn@gmail.com>
2026-04-06 18:45:11 +0530

    sync all files

--- app.py
+++ app.py
@@ -25,6 +25,12 @@ from opd_documentation import create_drug_master, import_large_drug_dataset
 from supabase_client import supabase
 
 
+from supabase import create_client
+
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 
 
